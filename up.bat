e53b69f3814c7d1bc274244d5b8c04e564f15232
Tobias <123253235+TobiasRuhmanseder@users.noreply.github.com>
2023-02-24 19:18:48 +0100

    upload

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
-@REM to upload into repositories, before that, the current version is updated with pull - just write "./up.bat" into the Terminal
+@REM to upload into repositories, before that, the current version is updated with pull - just write "./up.bat -commit text- into the Terminal
 
 
 git pull
