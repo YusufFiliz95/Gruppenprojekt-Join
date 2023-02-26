67ec23a1cbabecf6c1f3a8a23bb99e5102448f44
Tobias <123253235+TobiasRuhmanseder@users.noreply.github.com>
2023-02-26 19:22:37 +0100

    update bord.html js

--- javascript/board.js
+++ javascript/board.js
@@ -1,18 +1,16 @@
 function openAddTaskDialogBorder() {
     document.getElementById('overlay-bord-addTaskId').classList.remove('d-none');
     document.getElementById('bodyBordId').classList.add('overflow-dialog');
-    setTimeout(dialogComeInFromRightSide, 0);
-    /* dialogComeInFromRightSide(); */
+    document.getElementById('add-task-bordId').style.right = 'calc((100% - 85%)/2)';  /* Dialog return to the right outside */
+
+
 
 }
 
 function closeAddTaskDialogBorder() {
+    document.getElementById('add-task-bordId').style.right = '-130%';  /* Dialog return to the right outside */
     document.getElementById('overlay-bord-addTaskId').classList.add('d-none');
     document.getElementById('bodyBordId').classList.remove('overflow-dialog');
-    document.getElementById('add-task-bordId').style.right = '-100%'; /* Dialog return to the right outside */
 }
 
 
-function dialogComeInFromRightSide() {
-    document.getElementById('add-task-bordId').style.right = '0%';
-}
