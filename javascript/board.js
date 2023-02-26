d82ee55e01dff137e5b34e662fa4769075b13eee
Tobias <123253235+TobiasRuhmanseder@users.noreply.github.com>
2023-02-26 19:49:40 +0100

    update bord.html js

--- javascript/board.js
+++ javascript/board.js
@@ -1,9 +1,16 @@
+/* the Dialogs right outside (Addtask...)  runs in height with scroll */
+window.onscroll = function () {
+    let scrollY = window.scrollY;
+    document.getElementById('add-task-bordId').style.top = `calc(5% + ${scrollY}px`;
+}
+
+
 function openAddTaskDialogBorder() {
+
     document.getElementById('overlay-bord-addTaskId').classList.remove('d-none');
     document.getElementById('bodyBordId').classList.add('overflow-dialog');
-    document.getElementById('add-task-bordId').style.right = 'calc((100% - 85%)/2)';  /* Dialog return to the right outside */
-
-
+    document.getElementById('add-task-bordId').style.top = `calc(5% + ${scrollY}px`;
+    document.getElementById('add-task-bordId').style.right = `calc((100% - 85%)/2)`;  /* Dialog return to the right outside */
 
 }
 
@@ -12,5 +19,3 @@ function closeAddTaskDialogBorder() {
     document.getElementById('overlay-bord-addTaskId').classList.add('d-none');
     document.getElementById('bodyBordId').classList.remove('overflow-dialog');
 }
-
-
