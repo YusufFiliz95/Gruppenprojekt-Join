7268cd4e4b9731473567d8705c825b812468b09d
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-02-26 16:20:25 +0100

    made some improvements to the sign in form

--- javascript/login.js
+++ javascript/login.js
@@ -4,7 +4,7 @@ function showLock() {
 
     if (passwordInput.value === '') {
         passwordLock.innerHTML = /*html*/ `
-            <img id="lock" src="img/lock-logo.svg" alt="">
+            <img id="lock" src="img/lock_logo.svg" alt="">
         `;
     } else {
         passwordLock.innerHTML = /*html*/ `
@@ -31,10 +31,13 @@ function hidePassword() {
 
 function singUpForm() {
     document.getElementById('login').innerHTML = '';
-    document.getElementById('signupheader').innerHTML = '';
+    document.getElementById('signupheader').classList.add('d-none');
     document.getElementById('login').classList.add('sign-up-height');
     document.getElementById('signupbottomsection').classList.add('d-none');
     document.getElementById('login').innerHTML = /*html*/ `
+    <div class="back-btn">
+            <img src="img/back_btn.svg" alt="" onclick="goBackToLogIn()">
+        </div>
         <h1 class="sign-up-title">Sign Up</h1>
         <div class="log-in-border"></div>
         <div class="input-field user user-margin-top">
@@ -43,12 +46,12 @@ function singUpForm() {
         </div>
         <div class="input-field user">
             <input type="email" placeholder="Email">
-            <img src="img/email-logo.svg" alt="">
+            <img src="img/email_logo.svg" alt="">
         </div>
         <div class="input-field user">
             <input type="password" placeholder="Password" id="inputpassword" oninput="showLock()" required>
             <div class="password-lock" id="passwordlock">
-                <img id="lock" src="img/lock-logo.svg" alt="">
+                <img id="lock" src="img/lock_logo.svg" alt="">
             </div>
         </div>
         <button class="dark-btn sign-up-btn">Sign up</button>
@@ -56,5 +59,37 @@ function singUpForm() {
 }
 
 function goBackToLogIn(){
-    
+    document.getElementById('login').innerHTML = '';
+    document.getElementById('login').classList.remove('sign-up-height');
+    document.getElementById('signupheader').classList.remove('d-none');
+    document.getElementById('signupbottomsection').classList.remove('d-none');
+    document.getElementById('login').innerHTML = /*html*/ `
+                    <span class="log-in-title">Log in</span>
+                <div class="log-in-border"></div>
+                <div class="input-field email">
+                    <input type="email" placeholder="Email">
+                    <img src="img/email_logo.svg" alt="">
+                </div>
+                <div class="input-field">
+                    <input type="password" placeholder="Password" id="inputpassword" oninput="showLock()" required>
+                    <div class="password-lock" id="passwordlock">
+                        <img id="lock" src="img/lock_logo.svg" alt="">
+                    </div>
+                </div>
+                <div class="options">
+                    <div class="remember-me-option">
+                        <input type="checkbox" id="myCheckbox" name="myCheckbox" value="on" class="checkbox">
+                        <span>Remember me</span>
+                    </div>
+                    <div class="forgot-pw-option">
+                        <a href="#">Forgot my password</a>
+                    </div>
+                </div>
+                <div class="log-in-buttons">
+                    <button class="dark-btn log-in-btn">Log in</button>
+                    <button class="transparent-btn guest-log-in-btn">Guest Log in</button>
+                </div>
+            </div>
+        </div>
+    `;
 }
