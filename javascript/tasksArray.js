00bdb5872c3ff15149023f149bdab4d2794f7d68
Tobias <123253235+TobiasRuhmanseder@users.noreply.github.com>
2023-03-06 18:46:49 +0100

    2nd update after call

--- javascript/tasksArray.js
+++ javascript/tasksArray.js
@@ -1,3 +1,8 @@
+/* @ Christoph - wie lösen wir das mit der category-color, einfach mit einem RGB Code/ Hexcode ?? Wär glaub ich am einfachsten 
+    danach mit classList arbeiten - 
+    @ Yusuf dasselbe bei den contacts oder?*/
+
+
 let tasks = [
     {
         "title": "Einkaufen",
@@ -6,7 +11,18 @@ let tasks = [
         "category-color": "blau",
         "contacts": [],
         "date": "timestamp",
-        "prio": "Medium", 
+        "prio": 1,  /* 1 = Urgent  2 = Medium  3= Low */
+        "subtasks": [],
+        "status": "toDo"
+    },
+    {
+        "title": "Wäsche waschen",
+        "discription": "Die Waschmaschine muss heute noch vollgas laufen",
+        "category": "Hausarbeit",
+        "category-color": "green",
+        "contacts": [],
+        "date": "timestamp",
+        "prio": 1,  /* 1 = Urgent  2 = Medium  3= Low */
         "subtasks": [],
         "status": "toDo"
     }
