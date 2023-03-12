e61b966a5f5c21df5213df02f3bc0e7ed148dda2
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-03-12 14:30:19 +0100

    made some little improvements

--- javascript/contacts.js
+++ javascript/contacts.js
@@ -3,7 +3,7 @@ const contacts = [
         'name': 'AMax',
         'surname': 'Mustermann',
         'email': 'max.mustermann@hotmail.com',
-        'color': ''
+        'color': '#ecab0'
     },
     {
         'name': 'BMax',
@@ -50,12 +50,11 @@ function loadContacts() {
     for (let i = 0; i < sortedContacts.length; i++) {
         const list = sortedContacts[i];
         const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0'); //Generates random colors
-
         // Check if the first character of the name is different from the previous contact
         if (list.name.charAt(0) != currentChar) {
             currentChar = list.name.charAt(0);
             document.getElementById('contactlist').innerHTML += /*html*/ `
-                <div class="contact-char">${currentChar}</div>
+                <div class="contact-char"><p> ${currentChar}</p></div>
             `;
         }
         // Render the contact
