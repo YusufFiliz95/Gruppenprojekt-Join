97ef276e49c4a6f2db17bcd07c8dac03dd1e7cc5
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-03-13 20:31:16 +0100

    improved contact list

--- javascript/contacts.js
+++ javascript/contacts.js
@@ -3,57 +3,85 @@ let contacts = [
         'name': 'AMax',
         'surname': 'Mustermann',
         'email': 'max.mustermann@hotmail.com',
-        'initials': 'AM',
-        'color': '7AE22A0'
+        'profilecolor': '#e04f3f',
+        'phonenumber': '012345789'
+    },
+    {
+        'name': 'AMax',
+        'surname': 'Mustermann',
+        'email': 'max.mustermann@hotmail.com',
+        'profilecolor': '#e04f3f',
+        'phonenumber': '012345789'
+    },
+    {
+        'name': 'AMax',
+        'surname': 'Mustermann',
+        'email': 'max.mustermann@hotmail.com',
+        'profilecolor': '#e04f3f',
+        'phonenumber': '012345789'
     },
     {
         'name': 'BMax',
         'surname': 'Mustermann',
         'email': 'max.mustermann@hotmail.com',
-        'initials': 'BM',
-        'color': 'FB6A45'
+        'profilecolor': '#564eab',
+        'phonenumber': '012345789'
     },
     {
-        'name': 'FMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann1@hotmail.com',
-        'initials': 'FM',
-        'color': '013DFF'
-    },
-    {
-        'name': 'HMax',
+        'name': 'BMax',
         'surname': 'Mustermann',
         'email': 'max.mustermann@hotmail.com',
-        'initials': 'HM',
-        'color': '43D300'
+        'profilecolor': '#564eab',
+        'phonenumber': '012345789'
     },
     {
-        'name': 'KMax',
+        'name': 'BMax',
         'surname': 'Mustermann',
         'email': 'max.mustermann@hotmail.com',
-        'initials': 'KM',
-        'color': ''
-    },
-    {
-        'name': 'JMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'initials': 'JM',
-        'color': '8AA4FF'
-    },
-    {
-        'name': 'OMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'initials': 'OM',
-        'color': 'FD8A01'
+        'profilecolor': '#564eab',
+        'phonenumber': '012345789'
     },
     {
         'name': 'CMax',
         'surname': 'Mustermann',
         'email': 'max.mustermann@hotmail.com',
-        'initials': 'CM',
-        'color': 'FB0101'
+        'profilecolor': '#239177',
+        'phonenumber': '012345789'
+    },
+    {
+        'name': 'CMax',
+        'surname': 'Mustermann',
+        'email': 'max.mustermann@hotmail.com',
+        'profilecolor': '#239177',
+        'phonenumber': '012345789'
+    },
+    {
+        'name': 'CMax',
+        'surname': 'Mustermann',
+        'email': 'max.mustermann@hotmail.com',
+        'profilecolor': '#239177',
+        'phonenumber': '012345789'
+    },
+    {
+        'name': 'DMax',
+        'surname': 'Mustermann',
+        'email': 'max.mustermann@hotmail.com',
+        'profilecolor': '#b983c5',
+        'phonenumber': '012345789'
+    },
+    {
+        'name': 'DMax',
+        'surname': 'Mustermann',
+        'email': 'max.mustermann@hotmail.com',
+        'profilecolor': '#b983c5',
+        'phonenumber': '012345789'
+    },
+    {
+        'name': 'DMax',
+        'surname': 'Mustermann',
+        'email': 'max.mustermann@hotmail.com',
+        'profilecolor': '#b983c5',
+        'phonenumber': '012345789'
     },
 ]
 
@@ -64,26 +92,25 @@ function loadContacts() {
     let currentChar = '';
     for (let i = 0; i < sortedContacts.length; i++) {
         const list = sortedContacts[i];
-        const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0'); //Generates random colors
         // Check if the first character of the name is different from the previous contact
         if (list.name.charAt(0) != currentChar) {
             currentChar = list.name.charAt(0);
             document.getElementById('contactlist').innerHTML += /*html*/ `
-                <div class="contact-char"><p> ${currentChar}</p></div>
+                <div class="contact-char"><p>${currentChar.charAt(0).toUpperCase()}</p></div>
             `;
         }
         // Render the contact
         document.getElementById('contactlist').innerHTML += /*html*/ `
-            <div class="contact" onclick="showContactInfo()">
-                <div class="contact-initials" style="background-color: ${color}">
-                    <p>${list.name.charAt(0)}</p>
-                    <p>${list.surname.charAt(0)}</p>
+            <div class="contact" onclick="showContactInfo(${i})">
+                <div class="contact-initials" style="background-color: ${list.profilecolor}">
+                    <p>${list.name.charAt(0).toUpperCase()}</p>
+                    <p>${list.surname.charAt(0).toUpperCase()}</p>
                 </div>
                 <div class="contact-name-email">
-                    <div class="contact-name">
-                        <p>${list.name}</p>
-                        <p>${list.surname}</p>
-                    </div>
+                <div class="contact-name">
+                    <p>${list.name.charAt(0).toUpperCase() + list.name.slice(1).toLowerCase()}</p>
+                    <p>${list.surname.charAt(0).toUpperCase() + list.surname.slice(1).toLowerCase()}</p>
+                </div>
                     <div class="contact-email">
                         <p>${list.email}</p>
                     </div>
@@ -94,3 +121,23 @@ function loadContacts() {
 }
 
 
+function showContactInfo(i) {
+    const contactinfo = contacts[i];
+    const showClickedContact = document.getElementById('contactinfo');
+    showClickedContact.innerHTML = /*html*/`
+    <div class="contact-info-initials-name">
+        <div class="contact-info-initials" style="background-color: ${contactinfo.profilecolor}">
+            <p>${contactinfo.name.charAt(0).toUpperCase()}</p>
+            <p>${contactinfo.surname.charAt(0).toUpperCase()}</p>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+    `;
+}
+
+/**
+ *   const color = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0'); //Generates random colors
+ * 
+ */
