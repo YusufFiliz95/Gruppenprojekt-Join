bda9b5a6726e253bf569598a89285026ba20b5a2
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-03-14 20:43:20 +0100

    designing the add contact form

--- javascript/contacts.js
+++ javascript/contacts.js
@@ -4,83 +4,7 @@ let contacts = [
         'surname': 'Mustermann',
         'email': 'max.mustermann@hotmail.com',
         'profilecolor': '#e04f3f',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'AMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#e04f3f',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'AMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#e04f3f',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'BMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#564eab',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'BMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#564eab',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'BMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#564eab',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'CMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#239177',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'CMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#239177',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'CMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#239177',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'DMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#b983c5',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'DMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#b983c5',
-        'phonenumber': '012345789'
-    },
-    {
-        'name': 'DMax',
-        'surname': 'Mustermann',
-        'email': 'max.mustermann@hotmail.com',
-        'profilecolor': '#b983c5',
+        'Initials': 'AM',
         'phonenumber': '012345789'
     },
 ]
@@ -103,8 +27,7 @@ function loadContacts() {
         document.getElementById('contactlist').innerHTML += /*html*/ `
             <div class="contact" onclick="showContactInfo(${i})">
                 <div class="contact-initials" style="background-color: ${list.profilecolor}">
-                    <p>${list.name.charAt(0).toUpperCase()}</p>
-                    <p>${list.surname.charAt(0).toUpperCase()}</p>
+                    <p>${list.Initials.toUpperCase()}</p>
                 </div>
                 <div class="contact-name-email">
                 <div class="contact-name">
@@ -133,7 +56,7 @@ function showContactInfo(i) {
         <div class="contact-info-name-add-task">
             <p class="contact-info-name">${contactinfo.name} ${contactinfo.surname}</p>
             <div class="contact-info-add-task" onclick="openAddTaskDialogBord(${i})">
-                <img src="img/add_task_for_contact.svg" alt="">
+                <div class="contact-plus-icon"></div>
                 <p>Add Task</p>
             </div>
         </div>
@@ -143,7 +66,7 @@ function showContactInfo(i) {
             <p>Contact Information</p>
         </div>
         <div class="edit-contact" onclick="editContact(${i})">
-            <img src="img/edit_contact_icon.svg" alt="">
+            <div class="pen-icon"></div>
             <p>Edit Contact</p>
         </div>
     </div>
@@ -165,6 +88,11 @@ function showContactInfo(i) {
     }
     highlightContact[i].classList.add('selected-contact-info');
 }
+
+function addNewContact(){
+    
+}
+
 
 
 
