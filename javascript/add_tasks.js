e606b6fa4560c1d07aaa246bfaea0e755459dae7
Christoph Haase <christophhaase2@gmx.de>
2023-03-19 09:20:23 +0100

    added form js

--- javascript/add_tasks.js
+++ javascript/add_tasks.js
@@ -1,5 +1,7 @@
 
-let categoryColor;
+let currentColor;
+let categoryColor = [];
+let categoryName = [];
 let pointColor = ['#8AA4FF', '#FB0101', '#43D300', '#FD8A01', '#E224BE', '#013DFF', '#33D7C1',];
 let subtasks = [];
 let prio = 0;
@@ -22,18 +24,52 @@ function openInputfield(id) {
 function closeInputfield(id) {
     document.getElementById('category-input').classList.add('d-none');
     document.getElementById(id).classList.remove('d-none');
+    document.getElementById('color-container').classList.add('d-none');
+    document.getElementById('category-input-field').value = '';
 
 }
 
-function renderCategory() {
+// category Input
+function addedCategory() {
     let categoryInput = document.getElementById('category-input-field').value;
-    let categoryContainer = document.getElementById('category-container');
+    categoryColor.push(currentColor);
+    categoryName.push(categoryInput);
+}
 
-    categoryContainer.innerHTML += /*html*/`
+function renderCategory() {
+    let categoryContainer = document.getElementById('category-container');
+    categoryContainer.innerHTML = '';
+
+    for (let i = 0; i < categoryName.length; i++) {
+        categoryContainer.innerHTML += /*html*/`
         <div class="category">
-        <span>${categoryInput}</span>
-        <div class="color-circle " style="background-color: ${'' + categoryColor}"></div>
+            <div class="category-name">
+                <span onclick="addToInput()">${categoryName[i]}</span>
+                <div class="color-circle" style="background-color: ${'' + categoryColor[i]}"></div>
+            </div>
+            <img onclick="deleteCategory(${i})"src="./img/black-x.svg" >
         </div>`
+    }
+}
+
+function addToInput() {
+    let selectedCategory = document.getElementById('selected-Category');
+    selectedCategory.innerText = '';
+    selectedCategory.innerHTML += /*html*/`
+    <div class="category">
+        <div class="category-name">
+            <span>${categoryName[i]}</span>
+            <div class="color-circle" style="background-color: ${'' + categoryColor[i]}"></div>
+        </div>
+        <img src="./img/black-x.svg" >
+    </div>`
+}
+
+function deleteCategory(i) {
+    categoryColor.splice(i, 1);
+    categoryName.splice(i, 1);
+    renderCategory();
+
 }
 
 function renderCategoryColors() {
@@ -41,7 +77,7 @@ function renderCategoryColors() {
     colorContainer.innerHTML = '';
 
     for (let i = 0; i < pointColor.length; i++) {
-        color = pointColor[i]
+        color = pointColor[i];
         colorContainer.innerHTML += /*html*/`
         <div onclick="addColor('${color}')" id="circle${i}" class="color-circle" style="background-color: ${'' + color}"></div>`
     }
@@ -50,10 +86,10 @@ function renderCategoryColors() {
 function addColor(color) {
     document.getElementById('currentColor').innerHTML = '';
     document.getElementById('currentColor').innerHTML += /*html*/ `<div class="color-circle" style="background-color: ${'' + color}"></div>`
-    categoryColor = color;
+    currentColor = color;
 }
 
-
+// Contacts
 function renderContacts() {
     let contactContainer = document.getElementById('contact-container');
     contactContainer.innerHTML = '';
@@ -69,30 +105,40 @@ function renderContacts() {
     }
 }
 
-
+// Prio Buttons
 function addPrio(prioValue) {
     resetPrio();
-
     if (prioValue == prio) {
         prioValue = 0;
         prio = 0;
     }
     if (prioValue == 1) {
-        document.getElementById('urgent-btn').classList.add('urgent-aktiv');
-        document.getElementById('urgent-image').style.filter = 'brightness(0) invert(1)';
-        prio = 1;
+        selectUrgent();
     }
     if (prioValue == 2) {
-        document.getElementById('medium-btn').classList.add('medium-aktiv');
-        document.getElementById('medium-image').style.filter = 'brightness(0) invert(1)';
-        prio = 2;
+        selectMedium();
     }
     if (prioValue == 3) {
-        document.getElementById('low-btn').classList.add('low-aktiv');
-        document.getElementById('low-image').style.filter = 'brightness(0) invert(1)';
-        prio = 3;
+        selectLow();
     }
+}
 
+function selectUrgent() {
+    document.getElementById('urgent-btn').classList.add('urgent-aktiv');
+    document.getElementById('urgent-image').style.filter = 'brightness(0) invert(1)';
+    prio = 1;
+}
+
+function selectMedium() {
+    document.getElementById('medium-btn').classList.add('medium-aktiv');
+    document.getElementById('medium-image').style.filter = 'brightness(0) invert(1)';
+    prio = 2;
+}
+
+function selectLow() {
+    document.getElementById('low-btn').classList.add('low-aktiv');
+    document.getElementById('low-image').style.filter = 'brightness(0) invert(1)';
+    prio = 3;
 }
 
 function resetPrio() {
@@ -104,7 +150,7 @@ function resetPrio() {
     document.getElementById('low-image').style.filter = '';
 }
 
-
+// subtasks
 function openSubtaskInput() {
     document.getElementById('subtask-plus').classList.add('d-none');
     document.getElementById('subtask-buttons').classList.remove('d-none');
@@ -117,13 +163,16 @@ function closeSubtaskInput() {
 
 }
 
+
 function addSubtasks() {
     subtaskName = document.getElementById('subtask-input').value;
     subtasks.push(subtaskName);
 
 }
 
+
 function renderSubtasks() {
+    closeSubtaskInput();
     document.getElementById('subtask-input').value = '';
     subtasksContainer = document.getElementById('subtasks-container')
     subtasksContainer.innerHTML = '';
@@ -142,3 +191,6 @@ function deleteSubtasks(i) {
     subtasks.splice(i, 1);
     renderSubtasks();
 }
+
+// Formvalidation
+
