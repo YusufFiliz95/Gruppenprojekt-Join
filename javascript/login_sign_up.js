73d4d82dc8b20e8991af242b99b4082a3a9a47e9
Yusuf Filiz <y.filiz.ch@gmail.com>
2023-03-30 16:24:33 +0200

    deleted sign_up.js data and put the code to the new login_sign_up.js data

--- javascript/login_sign_up.js
+++ javascript/login_sign_up.js
@@ -1,3 +1,41 @@
+function showLock() {
+    const passwordInput = document.getElementById('inputpassword');
+    const passwordLock = document.getElementById('passwordlock');
+
+    if (passwordInput.value === '') {
+        passwordLock.innerHTML = /*html*/ `
+            <img id="lock" src="img/lock_logo.svg" alt="">
+        `;
+    } else {
+        passwordLock.innerHTML = /*html*/ `
+            <div class="hide-password" onclick="showPassword()" id="showpassword"></div>
+        `;
+    }
+}
+
+function showPassword() {
+    const passwordInput = document.getElementById('inputpassword');
+    passwordInput.type = 'text';
+    document.getElementById('passwordlock').innerHTML = /*html*/ `
+        <div class="show-password" onclick="hidePassword()" id="showpassword"></div>
+    `;
+}
+
+function hidePassword() {
+    const passwordInput = document.getElementById('inputpassword');
+    passwordInput.type = 'password';
+    document.getElementById('passwordlock').innerHTML = /*html*/ `
+        <div class="hide-password" onclick="showPassword()" id="showpassword"></div>
+    `;
+}
+
+
+function logInAsGuest(){
+    document.getElementById('user').innerHTML = 'Guest';
+}
+
+
+/*********************************SIGN UP*********************************/
 function singUpForm() {
     document.getElementById('login').innerHTML = '';
     document.getElementById('signupheader').classList.add('d-none');
@@ -68,3 +106,5 @@ function goBackToLogIn(){
 function signUp(){
     
 }
+
+/*********************************************************************************************************************/
